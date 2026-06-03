4197a667c16c28fe89f671fc951296f8a14cd332
Alvin Gosset <atom13d443gix@gmail.com>
2026-06-03 17:43:50 +0200

    pull request tp1 VoitureTest.java

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -11,7 +11,32 @@ public class VoitureTest {
 
     @Test
     void creerVoiture(){
-        assertEquals(1,1);
+        Voiture v = new Voiture("Peugeot", 20000);
+        assertEquals("Peugeot", v.getMarque());
+        assertEquals(20000, v.getPrix());
     }
 
+    @Test
+    void testModifications(){
+        Voiture v = new Voiture("Peugeot", 20000);
+        v.setMarque("Renault");
+        v.setPrix(15000);
+        assertEquals("Renault", v.getMarque());
+        assertEquals(15000, v.getPrix());
+    }
+
+    @Test
+    void testVoitureId(){
+        Voiture v = new Voiture("Peugeot", 20000);
+        assertEquals(0, v.getId());
+        v.setId(1);
+        assertEquals(1, v.getId());
+    }
+
+    @Test
+    void testToString(){
+        Voiture v = new Voiture("Peugeot", 20000);
+        v.setId(2);
+        assertEquals("Car{marque='Peugeot', prix=20000, id=2}", v.toString());
+    }
 }
