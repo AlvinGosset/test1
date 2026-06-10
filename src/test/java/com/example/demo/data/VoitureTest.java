c91688c27b4ff3c09a0b41f46cf2e73ff7445d52
Alvin Gosset <atom13d443gix@gmail.com>
2026-06-10 16:21:07 +0200

    amélioration tp1 dans main(oui js c pas bien)

--- src/test/java/com/example/demo/data/VoitureTest.java
+++ src/test/java/com/example/demo/data/VoitureTest.java
@@ -6,19 +6,25 @@ import org.springframework.util.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
+
 @SpringBootTest
 public class VoitureTest {
 
+    private Voiture v;
+    @BeforeEach
+    void setUp() {
+        v = new Voiture("Peugeot", 20000);
+    }
+
     @Test
     void creerVoiture(){
-        Voiture v = new Voiture("Peugeot", 20000);
         assertEquals("Peugeot", v.getMarque());
         assertEquals(20000, v.getPrix());
     }
 
     @Test
     void testModifications(){
-        Voiture v = new Voiture("Peugeot", 20000);
         v.setMarque("Renault");
         v.setPrix(15000);
         assertEquals("Renault", v.getMarque());
@@ -27,7 +33,6 @@ public class VoitureTest {
 
     @Test
     void testVoitureId(){
-        Voiture v = new Voiture("Peugeot", 20000);
         assertEquals(0, v.getId());
         v.setId(1);
         assertEquals(1, v.getId());
@@ -35,7 +40,6 @@ public class VoitureTest {
 
     @Test
     void testToString(){
-        Voiture v = new Voiture("Peugeot", 20000);
         v.setId(2);
         assertEquals("Car{marque='Peugeot', prix=20000, id=2}", v.toString());
     }
