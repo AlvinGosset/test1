4fe7b93e69dd15b20a4c66ac8904b6d136f69779
Alvin Gosset <atom13d443gix@gmail.com>
2026-06-17 15:41:10 +0200

    commit des Tests de Statistique

--- src/test/java/com/example/demo/service/StatistiqueTests.java
+++ src/test/java/com/example/demo/service/StatistiqueTests.java
@@ -2,12 +2,38 @@ package com.example.demo.service;
 
 import com.example.demo.data.Voiture;
 import org.junit.jupiter.api.Test;
-
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-
+@SpringBootTest
 public class StatistiqueTests {
+    Statistique statistique;
+    @BeforeEach
+    void setUp(){
+        statistique = new StatistiqueImpl();
+    }
+
+    @Test
+    void testAjouter(){
+        Voiture voiture = new Voiture("Peugeot", 20000);
+        assertThrows(NullPointerException.class, () -> statistique.ajouter(null));
+        statistique.ajouter(voiture);
+        assertEquals(1, statistique.prixMoyen().getNombreDeVoitures());
+        assertEquals(20000, statistique.prixMoyen().getPrixMoyen());
+    }
+
+    @Test
+    void testPrixMoyen(){
+        Voiture voiture = new Voiture("Peugeot", 20000);
+        Voiture voiture2 = new Voiture("Renault", 15000);
+        assertThrows(ArithmeticException.class, () -> statistique.prixMoyen());
+        statistique.ajouter(voiture);
+        statistique.ajouter(voiture2);
+        assertEquals(2, statistique.prixMoyen().getNombreDeVoitures());
+        assertEquals(17500, statistique.prixMoyen().getPrixMoyen());
+    }
 
     
 
